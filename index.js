4bb9d1454abeb8f1ef10b2ec11a815238a5bc660
lokesh pandey <luvpnd@gmail.com>
2024-06-26 16:09:28 +0530

    cors change

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ import userRoutes from "./src/user/user.routes.js";
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://secure-auth-neon.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (req, res, next) => {
   res.send("Hello world");
